1fb0377ddcb4d0ee1fc991913c45e71dda7f11db
Des <desinoelle@gmail.com>
2019-01-04 19:57:57 -0500

    Removed HTML pages.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,27 +3,32 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-      res.render("index");
+    res.render("index");
   });
 
   // Load sign up page
   app.get("/signup", function(req, res) {
-      res.render("signup");
+    res.render("signup");
   });
 
   // Load home page
   app.get("/home", function(req, res) {
-      res.render("home");
+    res.render("home");
+  });
+
+  // Load global chat
+  app.get("/chat", function(req, res) {
+    res.render("chat");
   });
 
   // Load contact page
   app.get("/contact", function(req, res) {
-      res.render("contact");
+    res.render("contact");
   });
 
   // Load tictactoe page
   app.get("/tictactoe", function(req, res) {
-      res.render("tictactoe");
+    res.render("tictactoe");
   });
 
   // Render 404 page for any unmatched routes

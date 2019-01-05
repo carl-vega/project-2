56093c7221cae37987d0dc38113cd23719faa5f8
Andrew <8879789+manleyac@users.noreply.github.com>
2019-01-05 10:12:45 -0500

    update js

--- public/js/tictactoe.js
+++ public/js/tictactoe.js
@@ -1,37 +1,5 @@
-<<<<<<< HEAD
-let origBoard;
-const player1 = "O";
-const player2 = "X";
-=======
 //winning combinations
->>>>>>> 9a8930eb5cd035c3e99ee97c8309cbbd34dc811c
-const winCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
-];
 
-<<<<<<< HEAD
-const cells = document.querySelectorAll(".cell");
-startGame();
-
-function startGame() {}
-
-function newGame() {
-  document.querySelector(".endgame").style.display = "none";
-  origBoard = Array.from(Array(9).keys());
-  for (var i = 0; i < cells.length; i++) {
-    cells[i].innerText = "";
-    cells[i].style.removeProperty("background-color");
-    cells[i].addEventListener("click", turnClick, false);
-  }
-}
-=======
 function newBoard() {
    let gameArray = [];
    for(let i=0; i<9; i++) {
@@ -40,9 +8,32 @@ function newBoard() {
    return gameArray;
 }
 
-function checkWin() {
+function checkForWinner() {
+   var space1 = $("#board tr:nth-child(1) td:nth-child(1)").text();
+   var space2 = $("#board tr:nth-child(1) td:nth-child(2)").text();
+   var space3 = $("#board tr:nth-child(1) td:nth-child(3)").text();
+   var space4 = $("#board tr:nth-child(2) td:nth-child(1)").text();
+   var space5 = $("#board tr:nth-child(2) td:nth-child(2)").text();
+   var space6 = $("#board tr:nth-child(2) td:nth-child(3)").text();
+   var space7 = $("#board tr:nth-child(3) td:nth-child(1)").text();
+   var space8 = $("#board tr:nth-child(3) td:nth-child(2)").text();
+   var space9 = $("#board tr:nth-child(3) td:nth-child(3)").text();
+   // check rows
+   if      ((space1==space2) && (space2==space3)) { return space3; }
+   else if ((space4==space5) && (space5==space6)) { return space6; }	
+   else if ((space7==space8) && (space8==space9)) { return space9; }
+   // check columns
+   else if ((space1==space4) && (space4==space7)) { return space7; }
+   else if ((space2==space5) && (space5==space8)) { return space8; }
+   else if ((space3==space6) && (space6==space9)) { return space9; }
+   // check diagonals
+   else if ((space1==space5) && (space5==space9)) { return space9; }
+   else if ((space3==space5) && (space5==space7)) { return space7; }
+   // no winner
+   return -1;
+ }
 
-}
+
 function displayBoard(gameBoard) {
    $("#board tr td").each( (index) => {
       $("tr #"+index).text(gameBoard[index]);
@@ -57,21 +48,35 @@ function main() {
 
    //init Array for new game
    let gameBoard = newBoard();
+   displayBoard(gameBoard);
    console.log(gameBoard); 
+   $("#reset").click(function() {
+      main();
 
+   });
    $("#board tr td").click(function() {
       if (gameBoard[$(this).attr("id")] === "" && play) {
         if ((move%2)==1) {
          gameBoard[$(this).attr("id")] = "O";
          console.log(gameBoard);
-         displayBoard(gameBoard);   
-        } 
+         displayBoard(gameBoard);
+         if (checkForWinner()!=-1 && checkForWinner()!="") { 
+            if (checkForWinner()=="X") { $("#alert").text("Player 1 wins!"); }
+                 else { $("#alert").text("Player 2 wins!"); }
+                 play = false; 
+               }
+        }
         else { 
          gameBoard[$(this).attr("id")] = "X";
          console.log(gameBoard);
          displayBoard(gameBoard);
-        }
         move++;
+        if (checkForWinner()!=-1 && checkForWinner()!="") { 
+         if (checkForWinner()=="X") { $("#alert").text("Player 1 wins!"); }
+              else { $("#alert").text("Player 2 wins!"); }
+              play = false; 
+            }
+         }
       }
     });
 
@@ -79,10 +84,10 @@ function main() {
       if(!play) {
          $("#board tr td").text("");
       }
+      
    });
 }
 
 $(document).ready(function(){
    main();
 });
->>>>>>> 9a8930eb5cd035c3e99ee97c8309cbbd34dc811c
